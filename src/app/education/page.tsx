13f3a5a5adc1c303c3e2d467e1d8597aa6021a76
Anas2338 <mohdanus20@gmail.com>
2024-11-27 18:20:31 +0500

    .

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,19 +1,26 @@
 export default function Education (){
     return(
         <div className="w-auto h-auto  mx-[10px]  flex flex-col gap-10 py-10 lg:flex lg:flex-row lg:gap-11 lg:px-[80px]">
-            <div className="bg-[#194d8a] text-white rounded-xl h-auto w-auto pt-10 flex flex-col gap-10 lg:w-[600px] lg:pb-5">
+            <div className="bg-[#194d8a] pb-4 text-white rounded-xl h-auto w-auto pt-10 flex flex-col gap-10 lg:w-[600px] lg:pb-5">
                 <h1 className="font-serif font-extrabold text-center text-3xl underline decoration-double text-[#252B42] leading-[50px]">Matriculation</h1>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Group: Computer Science</h2>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">School: K.M.A Boys Secondary School</h2>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Year: 2018</h2>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Grade: A</h2>
             </div>
-            <div className="bg-[#194d8a] text-white rounded-xl h-auto w-auto pt-10 flex flex-col gap-10 lg:w-[600px] lg:pb-5">
+            <div className="bg-[#194d8a] pb-4 text-white rounded-xl h-auto w-auto pt-10 flex flex-col gap-10 lg:w-[600px] lg:pb-5">
                 <h1 className="font-serif font-extrabold text-center text-3xl underline decoration-double text-[#252B42] leading-[50px]">Intermediate</h1>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Group: Commerce</h2>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest ">College: Government College of Commerce </h2>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Year: 2020</h2>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Grade: B</h2>
+            </div>
+            <div className="bg-[#194d8a] pb-4 text-white rounded-xl h-auto w-auto pt-10 flex flex-col gap-10 lg:w-[600px] lg:pb-5">
+                <h1 className="font-serif font-extrabold text-center text-3xl underline decoration-double text-[#252B42] leading-[50px]">Graduation</h1>
+                <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Group: Commerce</h2>
+                <h2 className="font-sans pl-5 font-bold text-lg tracking-widest ">College: Government College of Commerce </h2>
+                <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Year: 2<sup>nd</sup></h2>
+                <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Status: Continue</h2>
             </div>
         </div>
     )

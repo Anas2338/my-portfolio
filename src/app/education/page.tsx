d388f8f51da583b12153db8a128ddb0fee0c656d
Anas2338 <mohdanus20@gmail.com>
2024-11-27 17:47:12 +0500

    .

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,6 +1,6 @@
 export default function Education (){
     return(
-        <div className="w-auto h-auto  mx-[20px] px-[100px] flex flex-col gap-10 py-10 lg:flex lg:flex-row lg:gap-11 lg:px-[80px]">
+        <div className="w-auto h-auto  mx-[10px]  flex flex-col gap-10 py-10 lg:flex lg:flex-row lg:gap-11 lg:px-[80px]">
             <div className="bg-[#194d8a] text-white rounded-xl h-auto w-auto pt-10 flex flex-col gap-10 lg:w-[600px] lg:pb-5">
                 <h1 className="font-serif font-extrabold text-center text-3xl underline decoration-double text-[#252B42] leading-[50px]">Matriculation</h1>
                 <h2 className="font-sans pl-5 font-bold text-lg tracking-widest">Group: Computer Science</h2>

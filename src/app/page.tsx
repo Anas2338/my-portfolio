152a15a4ac97814b861c485fb9d4fa3d3110326f
Anas2338 <mohdanus20@gmail.com>
2024-11-27 18:07:34 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home (){
   return(
     <div>
       <div className="w-auto   mx-9 flex flex-col-reverse items-center gap-10 py-[100px] lg:flex lg:flex-row lg:px-[50px] lg:gap-20 ">
-        <div className="flex flex-col  mx-[80px] lg:mx-[0px] gap-[40px]  lg:gap-[60px]">
+        <div className="flex flex-col  mx-[20px] lg:mx-[0px] gap-[40px]  lg:gap-[60px]">
           <h2 className="font-sans font-bold text-2xl lg:text-3xl  text-amber-600">Hello I`m</h2>
           <h1 className="font-sans font-extrabold text-4xl lg:text-5xl tracking-wide leading-[50px] text-white">Muhammad Anus</h1>
           <h3 className="font-sans font-bold text-lg lg:text-3xl text-cyan-600 ">Front-End Developer</h3>

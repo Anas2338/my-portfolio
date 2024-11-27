27e0014e9ad2efd860c3c6125ec5427dcd50dd51
Anas2338 <mohdanus20@gmail.com>
2024-11-28 00:22:26 +0500

    .

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -6,25 +6,25 @@ import webdevelopment  from "../../../image/web development.png";
 export default function Skill (){
     return(
         <div className="w-auto flex flex-col gap-8 md:grid md:grid-cols-2  md:justify-items-center lg:grid lg:grid-cols-3 lg:px-[40px] lg:py-[80px]  mx-[40px] items-center py-[40px] ">
-            <div  className="flex flex-col bg-[#2982b6] w-[328] h-[292] py-[35] px-[40] gap-[20] rounded-2xl">
-              <div  className=" w-[100px] h-[100px]  gap-[10]  "  ><Image className="rounded-[10] " src={trading} alt="trading logo"></Image></div>
+            <div  className="flex flex-col bg-[#2982b6] w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] rounded-2xl">
+              <div  className=" w-[100px] h-[100px]  gap-[10]  "  ><Image className="rounded-[10px] " src={trading} alt="trading logo"></Image></div>
               <div  className="w-[135px] h-[24px] "><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap ">Forex Trading</h3></div>
-              <div  className=" w-[222] h-[60]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Proficient in forex trading, technical analysis, risk management, and strategies.</p></div>
+              <div  className=" w-[222px] h-[60px]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Proficient in forex trading, technical analysis, risk management, and strategies.</p></div>
             </div>
-            <div  className="flex flex-col bg-[#2982b6] w-[328] h-[292] py-[35] px-[40] gap-[20] rounded-2xl">
-              <div  className=" w-[100] h-[100]   gap-[10] "  ><Image className="rounded-[10]" src={stenography} alt="stenography logo"></Image></div>
-              <div  className="w-[135] h-[24]"><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap">Stenography</h3></div>
-              <div  className="w-[222] h-[60]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Skilled in stenography, fast typing, transcription, and accurate note-taking.</p></div>
+            <div  className="flex flex-col bg-[#2982b6] w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] rounded-2xl">
+              <div  className=" w-[100px] h-[100px]   gap-[10px] "  ><Image className="rounded-[10px]" src={stenography} alt="stenography logo"></Image></div>
+              <div  className="w-[135px] h-[24px]"><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap">Stenography</h3></div>
+              <div  className="w-[222px] h-[60px]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Skilled in stenography, fast typing, transcription, and accurate note-taking.</p></div>
             </div>
-            <div  className="flex flex-col bg-[#2982b6] w-[328] h-[292] py-[35] px-[40] gap-[20] rounded-2xl">
-              <div  className=" w-[100] h-[100]  gap-[10] " ><Image className="rounded-[10]" src={repairing} alt="repairing logo"></Image></div>
-              <div  className="w-[135] h-[24]"><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap">Electrical Repairing</h3></div>
-              <div  className="w-[222] h-[60]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Expert in electrical repairing, troubleshooting, maintenance, and equipment installation.</p></div>
+            <div  className="flex flex-col bg-[#2982b6] w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] rounded-2xl">
+              <div  className=" w-[100px] h-[100px]  gap-[10px] " ><Image className="rounded-[10px]" src={repairing} alt="repairing logo"></Image></div>
+              <div  className="w-[135px] h-[24px]"><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap">Electrical Repairing</h3></div>
+              <div  className="w-[222px] h-[60px]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Expert in electrical repairing, troubleshooting, maintenance, and equipment installation.</p></div>
             </div>
-            <div  className="flex flex-col bg-[#2982b6] w-[328] h-[292] py-[35] px-[40] gap-[20] rounded-2xl">
-              <div  className=" w-[100] h-[100]   gap-[10] "  ><Image className="rounded-[10]" src={webdevelopment} alt="web development logo"></Image></div>
-              <div  className="w-[135] h-[24]"><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap">Web Development</h3></div>
-              <div  className="w-[222] h-[60]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Experienced in web development, HTML, CSS, JavaScript, and responsive design.</p></div>
+            <div  className="flex flex-col bg-[#2982b6] w-[328px] h-[292px] py-[35px] px-[40px] gap-[20px] rounded-2xl">
+              <div  className=" w-[100px] h-[100px]   gap-[10px] "  ><Image className="rounded-[10px]" src={webdevelopment} alt="web development logo"></Image></div>
+              <div  className="w-[135px] h-[24px]"><h3 className="font-sans font-bold text-[#252B42] tracking-widest leading-6 text-base text-nowrap">Web Development</h3></div>
+              <div  className="w-[222px] h-[60px]"><p className="font-serif text-[#FFFFFF] text-sm leading-5 tracking-wide">Experienced in web development, HTML, CSS, JavaScript, and responsive design.</p></div>
             </div>
         </div>
     )
